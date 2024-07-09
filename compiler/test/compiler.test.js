68b0ea82737829807c2ee33d6ea30093ecf8d420
Debatreya <debatreyadas@gmail.com>
2024-07-09 15:42:47 +0530

    On testing

--- compiler/test/compiler.test.js
+++ compiler/test/compiler.test.js
@@ -1,7 +1,23 @@
 const formily_flowise_compiler = require('../compiler');
 const fs = require('fs');
 
-// Test case 1 (INPUT FIELD)
+// Test case 1 (INPUT FIELD | SINGLE)
 const formily_input1 = fs.readFileSync('./formily/formily_input1.json');
 const flowise_output1 = formily_flowise_compiler(JSON.parse(formily_input1))
 fs.writeFileSync('./flowise/flowise_output1.json', JSON.stringify(flowise_output1, null, 2));
+
+// Test case 2 (INPUT FIELD | MULTIPLE)
+const formily_input2 = fs.readFileSync('./formily/formily_input2.json');
+const flowise_output2 = formily_flowise_compiler(JSON.parse(formily_input2))
+fs.writeFileSync('./flowise/flowise_output2.json', JSON.stringify(flowise_output2, null, 2));
+
+// Test case 3 (SELECT FIELD | SINGLE)
+const formily_select1 = fs.readFileSync('./formily/formily_select1.json');
+const flowise_select1 = formily_flowise_compiler(JSON.parse(formily_select1));
+fs.writeFileSync('./flowise/flowise_select1.json', JSON.stringify(flowise_select1, null, 2));
+
+// Test case 4 (SELECT FIELD | MULTIPLE)
+
+// Test case 5 (MIX)
+
+// Sapce separated field (SELECT, INPUT, mix)
